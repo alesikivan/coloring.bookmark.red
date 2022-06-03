afee1c7ff9e42824d3153433e307456dedc562ac
alesikivan <obcofficiall@gmail.com>
2022-06-03 18:09:29 +0300

    app init

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: './core/client.js',
+    env : {
+      'PORT': 80,
+      'NODE_ENV' : 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : ['45.93.136.98'],
+      ref  : 'origin/main',
+      repo : 'https://github.com/alesikivan/client-bookmark-red',
+      path : '/root/apps/bookmark-client',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
