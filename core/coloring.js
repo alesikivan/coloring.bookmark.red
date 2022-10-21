982abb4cf9f570b8ee5b66f7b80ebc2ac92c907a
alesikivan <obcofficiall@gmail.com>
2022-10-21 15:07:54 +0300

    feat (app): added PDF & recording import/export

--- core/coloring.js
+++ core/coloring.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
+// const cors = require('cors')
+
 const app = express()
+
+// app.use(express.json({limit: '100mb'}));
+// app.use(express.urlencoded({extended: true}))
+// app.use(cors())
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 })
 
